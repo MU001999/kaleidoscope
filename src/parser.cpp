bef6c8fb828d3de645d320f67371d9f06ff85445
mu001999 <mu001999@outlook.com>
2019-07-27 10:32:50 +0800

    add parser files

---
+++ src/parser.cpp
@@ -0,0 +1,6 @@
+#include "parser.hpp"
+
+namespace kaleidoscope
+{
+
+} // namespace kaleidoscope
---
+++ src/parser.hpp
@@ -0,0 +1,12 @@
+#ifndef KALEIDOSCOPE_PARSER_HPP
+#define KALEIDOSCOPE_PARSER_HPP
+
+namespace kaleidoscope
+{
+class Parser
+{
+
+};
+} // namespace kaleidoscope
+
+#endif // KALEIDOSCOPE_PARSER_HPP
