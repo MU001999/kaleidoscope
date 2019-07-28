3360962b7ac163854555815b9abc25c506fb9047
mu001999 <mu001999@outlook.com>
2019-07-28 14:00:25 +0800

    init parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,122 @@
+#include <string>
+#include <vector>
+#include <utility>
+#include <unordered_set>
+
 #include "parser.hpp"
+
+using namespace std;
+
+namespace
+{
+
+} // namespace
 
 namespace kaleidoscope
 {
+unique_ptr<ExprAST> Parser::log_error(const char *str)
+{
+    fprintf(stderr, "LogError: %s\n", str);
+    return nullptr;
+}
 
+unique_ptr<PrototypeAST> Parser::log_error_p(const char *str)
+{
+    log_error(str);
+    return nullptr;
+}
+
+void Parser::main_loop()
+{
+    fprintf(stderr, "ready> ");
+    get_next_token();
+    while (true)
+    {
+        switch (cur_token_)
+        {
+        case Token::END:
+            return;
+        case ';':
+            get_next_token();
+            break;
+        case Token::DEF:
+            handle_definition();
+            break;
+        case Token::EXTERN:
+            handle_extern();
+            break;
+        default:
+            handle_top_level_expression();
+            break;
+        }
+        fprintf(stderr, "ready> ");
+    }
+}
+
+Token Parser::get_next_token()
+{
+    return cur_token_ = lexer_.next();
+}
+
+unique_ptr<ExprAST> Parser::parse_expression()
+{
+
+}
+
+unique_ptr<ExprAST> Parser::parse_primary()
+{
+
+}
+
+unique_ptr<ExprAST> Parser::parse_number_expr()
+{
+    auto result = make_unique<NumberExprAST>(stod(cur_token_.value()));
+    get_next_token();
+    return result;
+}
+
+unique_ptr<ExprAST> Parser::parse_paren_expr()
+{
+
+}
+
+unique_ptr<ExprAST> Parser::parse_identifier_expr()
+{
+
+}
+
+unique_ptr<PrototypeAST> Parser::parse_extern()
+{
+
+}
+
+unique_ptr<PrototypeAST> Parser::parse_prototype()
+{
+
+}
+
+unique_ptr<FunctionAST> Parser::parse_definition()
+{
+
+}
+
+unique_ptr<FunctionAST> Parser::parse_top_level_expr()
+{
+
+}
+
+void Parser::handle_definition()
+{
+
+}
+
+void Parser::handle_extern()
+{
+
+}
+
+void Parser::handle_top_level_expression()
+{
+
+}
 } // namespace kaleidoscope
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,11 +1,43 @@
 #ifndef KALEIDOSCOPE_PARSER_HPP
 #define KALEIDOSCOPE_PARSER_HPP
 
+#include <memory>
+
+#include "node.hpp"
+#include "lexer.hpp"
+
 namespace kaleidoscope
 {
 class Parser
 {
+  public:
+    Parser() = default;
+    ~Parser() = default;
 
+    void main_loop();
+    Token get_next_token();
+
+  private:
+    std::unique_ptr<ExprAST> log_error(const char *str);
+    std::unique_ptr<PrototypeAST> log_error_p(const char *str);
+
+    std::unique_ptr<ExprAST> parse_expression();
+    std::unique_ptr<ExprAST> parse_primary();
+    std::unique_ptr<ExprAST> parse_number_expr();
+    std::unique_ptr<ExprAST> parse_paren_expr();
+    std::unique_ptr<ExprAST> parse_identifier_expr();
+
+    std::unique_ptr<PrototypeAST> parse_extern();
+    std::unique_ptr<PrototypeAST> parse_prototype();
+    std::unique_ptr<FunctionAST> parse_definition();
+    std::unique_ptr<FunctionAST> parse_top_level_expr();
+
+    void handle_definition();
+    void handle_extern();
+    void handle_top_level_expression();
+
+    Lexer lexer_;
+    Token cur_token_;
 };
 } // namespace kaleidoscope
 
