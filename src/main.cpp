d16e2e2ecda392504d60016dd742014da4270074
mu001999 <mu001999@outlook.com>
2019-07-29 22:38:58 +0800

    update support for JIT but not complete

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,15 @@ using namespace kaleidoscope;
 
 int main()
 {
+    InitializeNativeTarget();
+    InitializeNativeTargetAsmPrinter();
+    InitializeNativeTargetAsmParser();
+
+    TheJIT = llvm::make_unique<orc::KaleidoscopeJIT>();
     initialize_module_and_pass_manager();
+
     Parser().main_loop();
     TheModule->print(errs(), nullptr);
+
     return 0;
 }
